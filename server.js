ea0b28d9873022e9e47f17c2005db4603f085b52
Kdddru <125441996+Kdddru@users.noreply.github.com>
2023-07-20 23:35:56 +0900

    0720 환경변수 추가

--- server.js
+++ server.js
@@ -4,26 +4,63 @@ const app = express();
 
 
 //listen (서버를 띄울 포트 번호, 띄운 후 실행할 코드)
-app.listen(3000,function(){
+app.listen(3000, function () {
   console.log('listening on 3000');
 });
 
 /************************************** */
 
-app.get('/',function(요청, 응답){
+app.get('/', function (요청, 응답) {
   응답.sendFile(__dirname + '/index.html');
 })
 
-app.get('/pet',function(요청, 응답){
+app.get('/pet', function (요청, 응답) {
   응답.send('펫용품 쇼핑할 수 있는 페이지 입니다.');
 })
 
-app.get('/beauty',function(요청, 응답){
+app.get('/beauty', function (요청, 응답) {
   응답.send('뷰티 용품 쇼핑페이지');
 })
 
+
+//파라미터, 쿼리 값 이용
+app.get('/user/:id',function(req,res){
+  //const q = req.params;
+  //console.log(q.id)
+
+  const q = req.query
+  console.log(q)
+
+  res.json({
+    'userid' : q.id
+  })
+})
+
+
+//api 
+app.get('/sound/:name',function(req,res){
+
+  const {name} = req.params;
+
+  if(name === '고양이'){
+    console.log(name);
+    res.json({'sound' : '야옹'})
+  }
+  else{
+    console.log(name);
+    console.log('고양이가 아님')
+  }
+})
+
+
+
+// 백엔드
+
+require('dotenv').config();
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dhk9817:doo980203@cluster0.cvr6pdo.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -36,7 +73,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const db = client.db('User');
     const coll = db.collection('User');
 
